21ff4c4cd6ee74fe7383cd7d70e9707c53f6f8f6
schaslivchik <schaslivchikm@gmail.com>
2020-06-18 16:22:56 +0300

    updated second block

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 
-$(".tiltBox").tilt({
-  maxTilt: 20,
-  perspective: 1400,
-  easing: "cubic-bezier(.03,.98,.52,.99)",
-  speed: 800,
-  glare: true,
-  maxGlare: 0.3,
-  scale: 1
-});
+// $(".tiltBox").tilt({
+//   maxTilt: 20,
+//   perspective: 1400,
+//   easing: "cubic-bezier(.03,.98,.52,.99)",
+//   speed: 800,
+//   glare: true,
+//   maxGlare: 0.3,
+//   scale: 1
+// });
 
 
 $('.tiltBox').hover(
