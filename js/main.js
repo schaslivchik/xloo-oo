149bfe2c2bdae488f179069c1a2b24a508a8bde4
schaslivchik <schaslivchikm@gmail.com>
2020-06-25 15:29:40 +0300

    updated

--- js/main.js
+++ js/main.js
@@ -58,3 +58,17 @@ $(document).keyup(function (e) {
     $('.xloo-modal').removeClass('modal-open');
   }
 });
+
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+
+function autosize() {
+  var el = this;
+  setTimeout(function () {
+    el.style.cssText = 'height:auto; padding:10px';
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  }, 0);
+}
