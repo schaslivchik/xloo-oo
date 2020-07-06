74230b16ce4700e04bea942fc88e3558e9c7edfb
schaslivchik <schaslivchikm@gmail.com>
2020-07-06 11:49:19 +0300

    updated

--- js/main.js
+++ js/main.js
@@ -1,13 +1,34 @@
+$(document).ready(function () {
+  $('#pagepiling').pagepiling({
+    menu: null,
+    direction: 'vertical',
+    verticalCentered: true,
+    sectionsColor: ['#000000', '#000000', '#ffffff', '#000000', '#000', '#ffffff'],
+    anchors: [],
+    scrollingSpeed: 300,
+    easing: 'swing',
+    loopBottom: false,
+    loopTop: false,
+    css3: true,
+    navigation: {
+      'textColor': '#000',
+      'bulletsColor': '#000',
+      'position': 'right',
+      'tooltips': ['section1', 'section2', 'section3', 'section4']
+    },
+    normalScrollElements: null,
+    normalScrollElementTouchThreshold: 5,
+    touchSensitivity: 5,
+    keyboardScrolling: true,
+    sectionSelector: '.section',
+    animateAnchor: false,
 
-// $(".tiltBox").tilt({
-//   maxTilt: 20,
-//   perspective: 1400,
-//   easing: "cubic-bezier(.03,.98,.52,.99)",
-//   speed: 800,
-//   glare: true,
-//   maxGlare: 0.3,
-//   scale: 1
-// });
+    //events
+    onLeave: function (index, nextIndex, direction) { },
+    afterLoad: function (anchorLink, index) { },
+    afterRender: function () { },
+  });
+});
 
 var owl = $('.owl-carousel');
 owl.owlCarousel({
@@ -25,13 +46,13 @@ $('.tiltBox').hover(
   function () { $(this).removeClass('tiltZindex') }
 )
 
-$(window).scroll(function () {
+/*$(window).scroll(function () {
   if ($(this).scrollTop() > 1) {
     $('.xloo-nav').addClass("xloo-sticky-nav");
   } else {
     $('.xloo-nav').removeClass("xloo-sticky-nav");
   }
-});
+});*/
 
 $('.main-menu-trigger').click(function () {
   event.preventDefault();
