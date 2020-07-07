9c75401c683e70aeb8c0dab8c2dda0b900cca2ac
schaslivchik <schaslivchikm@gmail.com>
2020-07-07 09:05:14 +0300

    init

--- js/main.js
+++ js/main.js
@@ -1,33 +1,107 @@
-$(document).ready(function () {
-  $('#pagepiling').pagepiling({
-    menu: null,
-    direction: 'vertical',
-    verticalCentered: true,
-    sectionsColor: ['rgba(44, 53, 68, 1)', '#000000', '#ffffff', '#000000', '#000', '#ffffff'],
-    anchors: [],
-    scrollingSpeed: 300,
-    easing: 'swing',
-    loopBottom: false,
-    loopTop: false,
-    css3: true,
-    navigation: {
-      'textColor': '#000',
-      'bulletsColor': '#505c6f',
-      'position': 'right',
-      'tooltips': ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7']
-    },
-    normalScrollElements: null,
-    normalScrollElementTouchThreshold: 5,
-    touchSensitivity: 5,
-    keyboardScrolling: true,
-    sectionSelector: '.section',
-    animateAnchor: false,
+// $(document).ready(function () {
+//   $('#pagepiling').pagepiling({
+//     menu: null,
+//     direction: 'vertical',
+//     verticalCentered: true,
+//     sectionsColor: ['rgba(44, 53, 68, 1)', '#000000', '#ffffff', '#000000', '#000', '#ffffff'],
+//     anchors: [],
+//     scrollingSpeed: 300,
+//     easing: 'swing',
+//     loopBottom: false,
+//     loopTop: false,
+//     css3: true,
+//     navigation: {
+//       'textColor': '#000',
+//       'bulletsColor': '#505c6f',
+//       'position': 'right',
+//       'tooltips': ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7']
+//     },
+//     normalScrollElements: null,
+//     normalScrollElementTouchThreshold: 5,
+//     touchSensitivity: 5,
+//     keyboardScrolling: true,
+//     sectionSelector: '.section',
+//     animateAnchor: false,
 
-    //events
-    onLeave: function (index, nextIndex, direction) { },
-    afterLoad: function (anchorLink, index) { },
-    afterRender: function () { },
-  });
+//     //events
+//     onLeave: function (index, nextIndex, direction) { },
+//     afterLoad: function (anchorLink, index) { },
+//     afterRender: function () { },
+//   });
+// });
+
+var myFullpage = new fullpage('#fullpage', {
+  licenseKey: '13AD7F77-A47C4568-AB432005-993C74FE',
+  //Навигация
+  menu: '#menu',
+  lockAnchors: false,
+  anchors: ['firstPage', 'secondPage', 'thirdPage', '4page', '5page', '6page', '7page'],
+  navigation: true,
+  navigationPosition: 'right',
+  navigationTooltips: ['firstSlide', 'secondSlide'],
+  showActiveTooltip: false,
+  slidesNavigation: false,
+  slidesNavPosition: 'bottom',
+
+  //Скроллинг
+  css3: true,
+  scrollingSpeed: 700,
+  autoScrolling: true,
+  fitToSection: true,
+  fitToSectionDelay: 1000,
+  scrollBar: false,
+  easing: 'easeInOutCubic',
+  easingcss3: 'ease',
+  loopBottom: false,
+  loopTop: false,
+  loopHorizontal: true,
+  continuousVertical: false,
+  continuousHorizontal: false,
+  interlockedSlides: false,
+  offsetSections: false,
+  resetSliders: false,
+  fadingEffect: true,
+  normalScrollElements: '.pp-scrollable2',
+  scrollOverflow: true,
+  scrollOverflowReset: null,
+  scrollOverflowOptions: null,
+  touchSensitivity: 15,
+  bigSectionsDestination: null,
+
+  //Доступ
+  keyboardScrolling: true,
+  animateAnchor: true,
+  recordHistory: true,
+
+  //Дизайн
+  controlArrows: true,
+  verticalCentered: true,
+  sectionsColor: ['#ccc', '#fff'],
+  paddingTop: '3em',
+  paddingBottom: '10px',
+  fixedElements: '#header, .footer',
+  responsiveWidth: 0,
+  responsiveHeight: 0,
+  responsiveSlides: false,
+  parallaxOptions: { type: 'reveal', percentage: 62, property: 'translate' },
+  cards: false,
+  cardsOptions: { perspective: 100, fadeContent: true, fadeBackground: true },
+
+  //Настроить селекторы
+  sectionSelector: '.section',
+  slideSelector: '.slide',
+
+  lazyLoading: true,
+
+  //события
+  onLeave: function (origin, destination, direction) { },
+  afterLoad: function (origin, destination, direction) { },
+  afterRender: function () { },
+  afterResize: function (width, height) { },
+  afterReBuild: function () { },
+  afterResponsive: function (isResponsive) { },
+  afterSlideLoad: function (section, origin, destination, direction) { },
+  onSlideLeave: function (section, origin, destination, direction) { }
 });
 
 var owl = $('.owl-carousel');
