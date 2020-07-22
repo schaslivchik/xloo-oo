59850b11e8cc3b97a73ada2d992674aac1c515ba
schaslivchik <schaslivchikm@gmail.com>
2020-07-22 16:24:08 +0300

    added animation

--- js/main.js
+++ js/main.js
@@ -1,36 +1,3 @@
-
-// $(document).ready(function () {
-//   $('#pagepiling').pagepiling({
-//     menu: null,
-//     direction: 'vertical',
-//     verticalCentered: true,
-//     sectionsColor: ['rgba(44, 53, 68, 1)', '#000000', '#ffffff', '#000000', '#000', '#ffffff'],
-//     anchors: [],
-//     scrollingSpeed: 300,
-//     easing: 'swing',
-//     loopBottom: false,
-//     loopTop: false,
-//     css3: true,
-//     navigation: {
-//       'textColor': '#000',
-//       'bulletsColor': '#505c6f',
-//       'position': 'right',
-//       'tooltips': ['section1', 'section2', 'section3', 'section4', 'section5', 'section6', 'section7']
-//     },
-//     normalScrollElements: null,
-//     normalScrollElementTouchThreshold: 5,
-//     touchSensitivity: 5,
-//     keyboardScrolling: true,
-//     sectionSelector: '.section',
-//     animateAnchor: false,
-
-//     //events
-//     onLeave: function (index, nextIndex, direction) { },
-//     afterLoad: function (anchorLink, index) { },
-//     afterRender: function () { },
-//   });
-// });
-
 var myFullpage = new fullpage('#fullpage', {
   licenseKey: '13AD7F77-A47C4568-AB432005-993C74FE',
   //Навигация
@@ -223,3 +190,4 @@ $(document).ready(function () {
 })
 
 particlesJS("particles-js", { "particles": { "number": { "value": 60, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "src": "img/github.svg", "width": 100, "height": 100 } }, "opacity": { "value": 0.25, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 3, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.3, "width": 1 }, "move": { "enable": true, "speed": 3, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200, "duration": 0.4 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true }); var count_particles, stats, update; stats = new Stats; stats.setMode(0); stats.domElement.style.position = 'absolute'; stats.domElement.style.left = '0px'; stats.domElement.style.top = '0px'; document.body.appendChild(stats.domElement); count_particles = document.querySelector('.js-count-particles'); update = function () { stats.begin(); stats.end(); if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) { count_particles.innerText = window.pJSDom[0].pJS.particles.array.length; } requestAnimationFrame(update); }; requestAnimationFrame(update);;
+
